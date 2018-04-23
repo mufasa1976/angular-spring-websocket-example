d7daa968ed0cc5a82bc6d82256cca92e5f95b64d
mufasa1976 <mufasa1976@coolstuff.software>
2018-04-23 08:57:58 +0200

    extended routing

--- angular/src/app/authentication/authentication.module.ts
+++ angular/src/app/authentication/authentication.module.ts
@@ -5,6 +5,7 @@ import {LoginComponent} from './components/login.component';
 import {MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
 import {AuthenticationGuard} from "./guards/authentication-guard.service";
+import {AuthenticationRoutingModule} from "./authentication-routing.module";
 
 const declarations = [
   LoginComponent
@@ -15,6 +16,7 @@ const declarations = [
     CommonModule,
     FormsModule,
     HttpClientModule,
+    AuthenticationRoutingModule,
     MatButtonModule,
     MatCardModule,
     MatInputModule,
