2c7b59e676d9187efa9fc35bc1b0ddcffecfaf0c
mufasa1976 <mufasa1976@coolstuff.software>
2018-06-11 09:20:02 +0200

    allow from every origin

--- server/src/main/java/io/github/mufasa1976/angularspringwebsocket/example/config/WebSocketConfiguration.java
+++ server/src/main/java/io/github/mufasa1976/angularspringwebsocket/example/config/WebSocketConfiguration.java
@@ -16,6 +16,7 @@ public class WebSocketConfiguration extends AbstractSecurityWebSocketMessageBrok
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket/connect")
+                .setAllowedOrigins("*")
                 .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 
